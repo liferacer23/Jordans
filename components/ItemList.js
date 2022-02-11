f443250b277b36f2fe76df53f6d54e54d07f64b3
liferacer23 <binyamg23@gmail.com>
2022-02-11 15:20:55 +0300

    card-modification

--- components/ItemList.js
+++ components/ItemList.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 import Slider from "../components/Slider/Slider";
 import Header from "../components/Header";
+import Link from "next/link";
 export default function ItemList({ jordans }) {
   
   return (
@@ -10,17 +11,22 @@ export default function ItemList({ jordans }) {
       <div className={styles.container}>
         {jordans.map((data, index) => {
           return (
+            
             <div key={data._id} className={styles.itemContainer}>
               <div className={styles.itemHeader}>
-                <h3>{data.title}</h3>
-                <a className={styles.expand} href={`store/jordans/${data._id}`}>
-                  Details
-                </a>
+                <h2>{data.title}</h2>
+
               </div>
               <div className={styles.itemBody}>
-              <p className={styles.description}>{data.description}</p>
               <Slider shoes={data} />
+              <div className={styles.details}> 
+                <h3>Price ${data.prices[0]}</h3>
+                <a className={styles.expand} href={`store/jordans/${data._id}`}>
+                  BUY
+                </a>
+                </div>
               </div>
+             
             </div>
           );
         })}
